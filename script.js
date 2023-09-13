662571b30c73a607ccac8d66ec8b9212473c7829
Markus Kaljo <markus.kaljo@tptlive.ee>
2023-09-13 16:22:54 +0300

    add more js code

--- script.js
+++ script.js
@@ -55,4 +55,12 @@ console.log(array);
 console.log(array[0]);
 array[0] = 7;
 console.log(array);
-
+array = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+console.log(array);
+console.log(array[1][2]);
+array = [ [ [ [ [ 5 ] ] ] ] ];
+console.log(array[0][0][0][0][0]);
