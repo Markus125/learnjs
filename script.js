d3b5f03c97b38993f032e7269f4b6ba85dbae611
Markus Kaljo <markus.kaljo@tptlive.ee>
2023-09-13 16:06:42 +0300

    add js and html

---
+++ script.js
@@ -0,0 +1,48 @@
+var num = 2;
+
+console.log(num);
+
+num = num + 2;
+
+num = 5 + 2 * 4 / 6 - 6;
+
+num += 3;
+num -= 3;
+num *= 3;
+num /= 3;
+num++;
+num--;
+num = 13 % 2;
+num = 2;
+num = num**3;
+
+console.log(typeof num);
+let string = 'hello world';
+string += "!!!!!!";
+console.log(string);
+let bool = true;
+bool = false;
+
+bool = true && false;
+bool = true || false;
+bool = !bool;
+bool = false && true || true && !false;
+bool = true;
+num = 10;
+bool = 2 > num;
+bool = 2 < num;
+bool = 10 < num;
+bool = 10 == num;
+bool = 10 <= num;
+bool = 10 >= num;
+bool = 10 != num;
+bool = '10' === num;
+
+
+
+console.log(bool);
+console.log(typeof bool);
+
+let noValue = null;
+let noValue2;
+console.log(noValue2); //undefined
