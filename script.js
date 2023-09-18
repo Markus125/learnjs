93e2fc27a1718a8c02484a3e9c938690d9d93d84
Markus Kaljo <markus.kaljo@tptlive.ee>
2023-09-18 08:28:36 +0300

    add js script

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var num = 2;
+let num = 2;
 
 console.log(num);
 
@@ -7,24 +7,25 @@ num = num + 2;
 num = 5 + 2 * 4 / 6 - 6;
 
 num += 3;
-num -= 3;
-num *= 3;
-num /= 3;
+num -= 4;
+num *= 4;
+num /= 4;
 num++;
 num--;
 num = 13 % 2;
 num = 2;
 num = num**3;
-
 console.log(typeof num);
 let string = 'hello world';
-string += "!!!!!!";
+string += '!!!!!';
 console.log(string);
+console.log(typeof string);
 let bool = true;
 bool = false;
 
-bool = true && false;
-bool = true || false;
+
+bool = false && true;
+bool = false || true;
 bool = !bool;
 bool = false && true || true && !false;
 bool = true;
@@ -37,17 +38,16 @@ bool = 10 <= num;
 bool = 10 >= num;
 bool = 10 != num;
 bool = '10' === num;
-
-
+bool = '10' !== num; 
 
 console.log(bool);
 console.log(typeof bool);
 
 let noValue = null;
 let noValue2;
-console.log(noValue2); //undefined
+console.log(noValue2); // undefined
 num = 0 * -1;
-num = 0,1 + 0,2;
+num = 0.1 + 0.2;
 console.log(num);
 let array = [1, 2, 3, 4];
 array = [1, 'hello', true, undefined, null];
@@ -64,3 +64,16 @@ console.log(array);
 console.log(array[1][2]);
 array = [ [ [ [ [ 5 ] ] ] ] ];
 console.log(array[0][0][0][0][0]);
+let obj = {
+    name: 'Kaspar',
+    age: 30,
+    eyeColor: 'blue',
+    favpets: ['dogs', 'cats', 'rats']
+};
+console.log(obj);
+console.log(obj.name);
+console.log(obj.favpets[1]);
+console.log(console);
+console.log(array.length);
+console.log(string.length);
+console.log(string[0]);
